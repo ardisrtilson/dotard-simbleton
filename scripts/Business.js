98d00b83fd3348a170014ec3dcff4561b9f4b928
ardisrtilson <email@example.com>
2020-07-16 11:56:20 -0500

    Further HTML adjustments

--- scripts/Business.js
+++ scripts/Business.js
@@ -1,17 +1,12 @@
 export const BusinessHTML = (businessObject) => {
     return `
         <section class="business">
-            <h2 class="business__type">${businessObject.purchasingAgent.nameLast}</h2>
-            <h2 class="business__type">${businessObject.purchasingAgent.nameFirst}</h2>
-            <h2 class="business__type">${businessObject.phoneWork}</h2>
-            <h2 class="business__type">${businessObject.orders}</h2>
-            <h2 class="business__type">${businessObject.companyName}</h2>
-            <h2 class="business__type">${businessObject.companyIndustry}</h2>
-            <h2 class="business__type">${businessObject.addressZipCode}</h2>
-            <h2 class="business__type">${businessObject.addressStateCode}</h2>
+            <h1 class="business__name">${businessObject.companyName}</h2>
+<frameset>
             <h2 class="business__type">${businessObject.addressFullStreet}</h2>
-            <h2 class="business__type">${businessObject.addressCity}</h2>
-        </section>
+            <h2 class="business__type">${businessObject.addressCity}, ${businessObject.addressStateCode} ${businessObject.addressZipCode}</h2>
+            </frameset>
+            </section>
     `
 }
 
