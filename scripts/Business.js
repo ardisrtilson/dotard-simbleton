be20ed6598ac37f6af0ee7e25c101905a7c4dfa1
ardisrtilson <email@example.com>
2020-07-16 16:44:31 -0500

    Added functional filter loop

--- scripts/Business.js
+++ scripts/Business.js
@@ -2,10 +2,10 @@ export const BusinessHTML = (businessObject) => {
     return `
         <section class="business">
             <h1 class="business__name">${businessObject.companyName}</h2>
-<frameset>
+            <fieldset>
             <h2 class="business__type">${businessObject.addressFullStreet}</h2>
             <h2 class="business__type">${businessObject.addressCity}, ${businessObject.addressStateCode} ${businessObject.addressZipCode}</h2>
-            </frameset>
+            </fieldset>
             </section>
     `
 }
--- scripts/BusinessList.js
+++ scripts/BusinessList.js
@@ -1,13 +1,22 @@
-import { useBusinesses } from "./BusinessProvider.js"
+import { useBusinesses} from "./BusinessProvider.js"
 import { BusinessHTML } from "./Business.js"
+import { businessesFiltered } from "./businessFilter.js"
 
 const contentTarget = document.querySelector(".dotard")
+const contentTarget2 = document.querySelector(".businessList--newYork")
 
 export const BusinessList = () => {
+    const filteredArray = businessesFiltered
     const businessesArray = useBusinesses()
     businessesArray.forEach(
         (businessObject) => {
             contentTarget.innerHTML += BusinessHTML(businessObject)
         }
     );
+    filteredArray.forEach(
+        (businessObject) => {
+            contentTarget2.innerHTML += BusinessHTML(businessObject)
+        }
+    );
 }
+
