685ae6fa81f997f8e8b38795497c09b3f1494006
ardisrtilson <email@example.com>
2020-07-17 11:21:12 -0500

    Improved search function

--- scripts/findBusiness.js
+++ scripts/findBusiness.js
@@ -3,5 +3,10 @@ import { useBusinesses } from "./BusinessProvider.js"
 const businesses = useBusinesses ()
 
 export const findByCompanyName = (searchCriteria) => {
-    return businesses.find(business => business.companyName = searchCriteria)
+    return businesses.find(business => {
+        return business.purchasingAgent.nameLast === searchCriteria ||
+            business.purchasingAgent.nameFirst === searchCriteria || 
+            business.companyName === searchCriteria 
+}
+)
 }
